ed6f845095035648f50c2066789a14ebb3862e27
jinrui <1029828759@qq.com>
2020-08-07 23:23:04 +0800

    add fm

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from core_layers import Fm_layer
+__all__ = ["Fm_layer",]
--- core_layers.py
+++ core_layers.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import tensorflow.keras as ks
 from tensorflow.keras.layers import Layer,Dense,Input, Embedding, LSTM,Bidirectional,Dropout,Activation,Convolution1D, Flatten, MaxPool1D, GlobalAveragePooling1D,BatchNormalization
 from tensorflow.keras.models import Model
-from keras import backend as K
+from tensorflow.keras import backend as K
 
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -15,9 +15,9 @@ import os
 
 #DNN
 class DNN_layers(Layer):
-    def __init__(self, hidden_units, activation='relu', l2_reg=0, dropout_rate=0, use_bn=False, seed=1024, **kwargs):
+    def __init__(self, hidden_units, activations, l2_reg=0, dropout_rate=0, use_bn=False, seed=1024, **kwargs):
         self.hidden_units = hidden_units
-        self.activation = activation
+        self.activations = activations
         self.dropout_rate = dropout_rate
         self.seed = seed
         self.l2_reg = l2_reg
@@ -34,18 +34,21 @@ class DNN_layers(Layer):
             kernel = self.add_weight(name='kernel_' + str(i),  shape=(last_dim, self.hidden_units[i]), initializer='glorot_uniform',trainable=True)
             self.kernels.append(kernel)
             last_dim = self.hidden_units[i]
-            bias = self.add_weight(name='bias_' + str(i),  shape=(self.hidden_units[i],), initializer='glorot_uniform',trainable=True)
+            bias = self.add_weight(name='bias_' + str(i),  shape=(self.hidden_units[i],), initializer='zeros',trainable=True)
             self.bias.append(bias)
+            self.built = True
         super(DNN_layers, self).build(input_shape)  # Be sure to call this somewhere!
 
     def call(self, x, training = False):
         deep_out = x
         for i in  range(len(self.kernels)):
             #x =  ks.layers.dot([x, kernel], axes =(-1,-1) )
-            x = tf.tensordot(deep_out, self.kernels[i],  axes =(-1,0) ) + self.bias[i]
-            x = tf.keras.layers.Activation(self.activation)(x)
-            x= tf.keras.layers.Dropout(self.dropout_rate)(x, training = training)
-            deep_out = x
+            #x = tf.tensordot(deep_out, self.kernels[i],  axes =(-1,0) ) + self.bias[i]
+            deep_out = K.dot(deep_out,self.kernels[i])
+            deep_out= K.bias_add(deep_out, self.bias[i], data_format='channels_last')
+            if  self.activations[i] and self.activations[i] is not None :
+                deep_out= tf.keras.layers.Activation(self.activations[i])(deep_out)
+            #x= tf.keras.layers.Dropout(self.dropout_rate)(x, training = training)
         return deep_out
 
     def compute_output_shape(self, input_shape):
@@ -53,14 +56,28 @@ class DNN_layers(Layer):
 
 #Fm_layer
 class Fm_layer(Layer):
-     def __init__(self, hidden_units, activation='relu', l2_reg=0, dropout_rate=0, use_bn=False, seed=1024, **kwargs):
-        self.hidden_units = hidden_units
-        self.activation = activation
-        self.dropout_rate = dropout_rate
-        self.seed = seed
-        self.l2_reg = l2_reg
-        self.use_bn = use_bn
+    def __init__(self, feat_num , vec_size,  l2_reg=0, **kwargs):
         super(Fm_layer, self).__init__(**kwargs)
+        self.l2_reg = l2_reg
+        self.feat_num = feat_num
+        self.vec_size = vec_size
+
+    def build(self, input_shape):
+        self.weight = self.add_weight(name='fm_weight',  shape=(self.feat_num, 1), initializer='glorot_uniform',trainable=True)
+        self.bias = self.add_weight(name='fm_bias',  shape=(1,), initializer='glorot_uniform',trainable=True)
+        self.fea_vec = self.add_weight(name='fm_vec',  shape=(self.feat_num, self.vec_size), initializer=ks.initializers.RandomUniform(minval=-0.05, maxval=0.05, seed=1024)
+,trainable=True)
+        super(Fm_layer, self).build(input_shape)  # Be sure to call this somewhere!
+
+    def call(self, x, training = None):
+        lr = tf.matmul(x,self.weight ) + self.bias
+        square_of_sum = tf.square(K.dot(x,  self.fea_vec))
+        sum_of_suqare = K.dot(K.square(x), K.square(self.fea_vec))
+        haha = 0.5 * tf.reduce_sum((square_of_sum - sum_of_suqare), axis = 1, keep_dims=True) + lr 
+        return   haha#th fm part has error ,make auc 0.5
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], 1)
 
 
 #BiInteraction_layer
@@ -84,9 +101,6 @@ class BiInteraction_layer(Layer):
 
 
     def call(self, x, training = False):
-        if K.nidm(x) != 3:
-            raise ValueError(
-                "Unexpected inputs dimensions %d, expect to be 3 dimensions" % (K.ndim(x)))
         square_of_sum = tf.square(tf.reduce_sum(x, axis = 1,keepdims = True))
         sum_of_suqare = tf.reduce_sum(tf.multiply(x, x), axis = 1,keepdims = True)
         return 0.5 * (square_of_sum - sum_of_suqare)
