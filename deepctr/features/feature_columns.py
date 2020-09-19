dcc9bca985b27f4d18703ee93b3e2f894add412e
jinrui <1029828759@qq.com>
2020-09-19 17:03:36 +0800

    fix bugs

--- deepctr/features/feature_columns.py
+++ deepctr/features/feature_columns.py
@@ -16,7 +16,7 @@ from tensorflow.keras.layers import Layer,Dense,Input, Embedding, LSTM,Bidirecti
 """
 把深度模型的特征定义为三种格式：1、离散->embedding  2、离散->onehot  3、连续->数字
 """
-class embeedding_columns():
+class embeedding_column():
     def __init__(name,  vocabulary_size,  embedding_dim, max_len = 1,use_hash=False, embeddings_initializer = None, trainable=True, dtype=tf.float32):
         self.name = name
         self.vocabulary_size = vocabulary_size
@@ -29,7 +29,7 @@ class embeedding_columns():
         if self.embeddings_initializer is None:
             self.embeddings_initializer  = RandomNormal(mean=0.0, stddev=0.0001, seed=1991)
 
-class numeric_columns():
+class numeric_column():
     def __init__(name, num_len = 1, dtype=tf.float32, normfun = None):
         self.name = name
         self.num_len = num_len
@@ -66,7 +66,43 @@ def dict_from_feature_columns(features, feature_columns):
             if fc.normfun is not None:
                  num_fea_dict[fc.name] = fc.normfun(features[fc.name])
             
-    return dnn_ fea_dict, num_fea_dict
+    return dnn_fea_dict, num_fea_dict
+    
+def conf_to_featurecolumns(path):
+    fea_list = []
+    feature_conf = os.path.join(path, 'feature_list.conf')
+    feature_map = os.path.join(path, 'feature_map.conf')
+    feature_columns = []
+    feature_columns_map = {}
+    for line in open(feature_conf):
+        if line.strip() == '' or line.strip().startswith('#'):
+            continue
+        lines = line.strip().split(';')
+        tmp_map = {}
+        for ll in lines:
+            ls = ll.split('=')
+            tmp_map[ls[0].strip()] = ls[1].strip()
+        fea_list.append(tmp_map)
+    for fea_col in fea_list:
+        fea_class = fea_col['class']
+        name = fea_col['name']
+        if fea_class == 'numeric_column':
+            num_len = 1
+            if 'num_len' in fea_col:
+                num_len = int(fea_col['num_len'])
+            col = numeric_column(name, num_len)
+        if fea_class == 'embedding_column':
+            col = embedding_column(name, vocabulary_size = int(fea_col['vocabulary_size'])\
+                , embedding_dim = int(fea_col['embedding_dim']), use_hash = fea_col['use_hash'] == 'true')
+        feature_columns.append(col)
+        feature_columns_map[name] = col
+    result = []
+    for line in open(feature_map):
+        lines = line.strip().split(',')
+        result.append([feature_columns_map[ll] for ll in lines])
+    return result
+
+
     
 
 
--- deepctr/models/deepfm.py
+++ deepctr/models/deepfm.py
@@ -15,6 +15,32 @@ from ..layers.layer_utils import build_input_layers
 def DeepFm(dnn_feature_columns,line_feature_columns,dnn_hidden_units=None,
     dnn_activation_fn=tf.nn.relu,dnn_dropout=None,output_activation = tf.nn.sigmoid,
     n_classes=1,batch_norm=False):
+    inputs_list = build_feature_columns(dnn_feature_columns + line_feature_columns)
+    dnn_fea_dict, num_fea_dict = dict_from_feature_columns(inputs_list, dnn_feature_columns + line_feature_columns)
+    line_input = tf.concat(num_fea_dict.values(), axis = 1)
+    #dnn_fea_dict 可能是多值特征,所以有一个reduce_mean
+    dnn_fea_list = [tf.reduce_mean(fea) for fea in dnn_fea_dict.values()]
+    dnn_input_fea = tf.concat(dnn_fea_list + num_fea_dict.values(), axis = 1)
+    lr_logit = Dense(1)(line_input) #lr
+    fm_input_fea = tf.reshape(dnn_input_fea, [-1, len(dnn_fea_list), dnn_fea_list[0].embedding_dim])
+    fm_logit = Fm()(fm_input_fea) #fm
+    blocks = ks.models.Sequential(name='dynamic-blocks')
+    for hit in dnn_hidden_units:
+        blocks.add(Dense(hit))
+        blocks.add(Activation(dnn_activation_fn))
+        if  dnn_dropout is not None:
+            blocks.add(Dropout(dnn_dropout))
+    deep_logit = blocks(dnn_input_fea) #dnn
+    all_concat = tf.concat([lr_logit, fm_logit, deep_logit], axis=1)
+    output = Dense(n_classes, Activation(output_activation))(all_concat)
+    myinputs = inputs_list.values()
+    model = tf.keras.models.Model(inputs=myinputs, outputs=output)
+    return model
+
+
+def DeepFm_v2(dnn_feature_columns,line_feature_columns,dnn_hidden_units=None,
+    dnn_activation_fn=tf.nn.relu,dnn_dropout=None,output_activation = tf.nn.sigmoid,
+    n_classes=1,batch_norm=False):
     dnn_input_layer = tf.keras.layers.DenseFeatures(
             feature_columns=dnn_feature_columns + line_feature_columns, name="dnn_input_layer")
     fm_input_layer = tf.keras.layers.DenseFeatures(
