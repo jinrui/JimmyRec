334f6e3cb837781b2be60b94ef8d3e6e9e07cd30
jinrui <1029828759@qq.com>
2021-05-12 18:01:54 +0800

    complete dssm

--- deepctr/models/dssm.py
+++ deepctr/models/dssm.py
@@ -6,10 +6,40 @@ import tensorflow.keras as ks
 from tensorflow.keras.layers import Layer,Dense,Input,DenseFeatures, Embedding, LSTM,Bidirectional,Dropout,Activation,Convolution1D, Flatten, MaxPool1D, GlobalAveragePooling1D,BatchNormalization
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
+from ..layers.core_layers import Attention_layer
+from ..layers.layer_utils import build_input_layers
+from ..features.feature_columns import build_feature_columns, dict_from_feature_columns
 
+def DSSM(user_feature_columns,item_feature_columns,user_dnn_hidden_units=None,item_dnn_hidden_units=None,
+    dnn_activation_fn=tf.nn.relu,dnn_dropout=None,output_activation = tf.nn.sigmoid,
+    n_classes=1,batch_norm=False):
+        inputs_list = build_feature_columns(user_feature_columns + item_feature_columns)
+        dnn_fea_dict, num_fea_dict = dict_from_feature_columns(inputs_list, user_feature_columns + item_feature_columns)
+        user_dnn_fea_list = [tf.reduce_mean(dnn_fea_dict[ufc.name], axis=1) for ufc in user_feature_columns if ufc.name in dnn_fea_dict]
+        item_dnn_fea_list = [tf.reduce_mean(dnn_fea_dict[ifc.name], axis=1) for ifc in item_feature_columns if ifc.name in dnn_fea_dict]
+        user_lr_fea_list = [num_fea_dict[ufc.name] for ufc in user_feature_columns if ufc.name in num_fea_dict]
+        item_lr_fea_list = [num_fea_dict[ifc.name] for ifc in item_feature_columns if ifc.name in num_fea_dict]
+        user_dnn_input_fea = tf.concat(user_dnn_fea_list + user_lr_fea_list, axis = -1)
+        item_dnn_input_fea = tf.concat(item_dnn_fea_list + item_lr_fea_list, axis = -1)
+        user_blocks = ks.models.Sequential(name='user_dynamic-blocks')
+        for hit in user_dnn_hidden_units:
+                user_blocks.add(Dense(hit))
+                user_blocks.add(Activation(dnn_activation_fn))
+        item_blocks = ks.models.Sequential(name='item_dynamic-blocks')
+        for hit in item_dnn_hidden_units:
+                item_blocks.add(Dense(hit))
+                item_blocks.add(Activation(dnn_activation_fn))
+        user_output = user_blocks(user_dnn_input_fea)
+        item_output = item_blocks(item_dnn_input_fea)
+        deep_logit = user_output * item_output #dnn
+        print(deep_logit)
+        output = Dense(n_classes, Activation(output_activation))(deep_logit)
+        myinputs = inputs_list.values()
+        model = tf.keras.models.Model(inputs=myinputs, outputs=output)
+        return model
 
 #使用dssm作为召回模型，很经典
-class Dssm(Model):
+class Dssm_v1(Model):
     def __init__(self, user_feature_columns, item_feature_columns ,dnn_hidden_units=None,\
     dnn_dropout = 0,output_activation = tf.nn.sigmoid, **kwargs):
         super().__init__(**kwargs)
