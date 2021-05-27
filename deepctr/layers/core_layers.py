57225c80d71ee0d4b6636e996510e2fd53d2dd59
jinrui <1029828759@qq.com>
2021-05-28 00:06:29 +0800

    add mmoe layer

--- deepctr/layers/core_layers.py
+++ deepctr/layers/core_layers.py
@@ -150,8 +150,8 @@ class Attention_layer(Layer):
 
     def build(self, input_shape):
         self.fc = tf.keras.Sequential()
-        for unit in self.hidden_units:
-            self.fc.add(layers.Dense(unit, activation=self.activation, name="fc_att_"+str(unit))) 
+        for i in range(len(self.hidden_units)):
+            self.fc.add(layers.Dense(self.hidden_units[i], activation=self.activation, name="fc_att_"+str(i))) 
         self.fc.add(layers.Dense(1, activation='softmax', name="fc_att_out"))
         super(Attention_layer, self).build(input_shape)  # Be sure to call this somewhere!
 
@@ -170,6 +170,50 @@ class Attention_layer(Layer):
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], 1)
+
+    #Attention_layer
+class MMOE_layer(Layer):
+    def __init__(self, expert_hidden_units, gate_hidden_units,num_experts, num_tasks, activation = 'relu', seed=1024, **kwargs):
+        self.experts = []
+        self.gates = []
+        self.num_experts = num_experts
+        self.num_tasks = num_tasks
+        self.activation = activation
+        super(MMOE_layer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        for i in range(self.num_experts):
+            fc = tf.keras.Sequential()
+            for j in range(len(self.expert_hidden_units)):
+                fc.add(layers.Dense(self.expert_hidden_units[j], activation=self.activation, name="fc_expert_"+str(i)+"_" + str(j))) 
+            self.experts.append(fc)
+        for i in range(self.num_tasks):
+            fc = tf.keras.Sequential()
+            for j in range(len(self.gate_hidden_units)):
+                fc.add(layers.Dense(self.gate_hidden_units[j], activation=self.activation, name="fc_gate_"+str(i)+"_" + str(j))) 
+            self.gates.append(fc)
+        super(MMOE_layer, self).build(input_shape)  # Be sure to call this somewhere!
+
+    def call(self, x, training = None):
+        experts_out = []
+        gates_out = []
+        for i in range(self.num_experts):
+            experts_out.append(tf.expand_dims(self.experts[i](x), axis = 2))
+        for i in range(self.num_tasks):
+            gates_out.append(tf.expand_dims(self.gates[i](x), axis = 1))
+        experts_out = tf.concat(experts_out, axis = 2) #batch_size * d * num_experts
+        gates_out = tf.concat(gates_out, axis = 1) # batch_size * num_tasks * d
+        weights_out = tf.matmul(gates_out, experts_out) # batch_size * num_tasks * num_experts
+        weights_out = tf.matmul(weights_out, tf.transpose(experts_out, [0, 2, 1])) #batch_size * num_tasks * d
+        return weights_out
+
+        
+
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], 1)
+
+
 
 
 
